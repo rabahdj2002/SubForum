7004e803fa206f7aab1d4fda0b5db4683858f18f
Rabah Djebbes <39097998+rabahdj2002@users.noreply.github.com>
2024-12-14 20:23:44 +0100

    .

--- subforum/subforum/views.py
+++ subforum/subforum/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def index(request):
     #return HttpResponse("Hello world!")
-    return render(request, "subforum/index.html")
+    return render(request, "subforum/new_prjct.html")
